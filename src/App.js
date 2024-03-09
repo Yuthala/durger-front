57ee17dc044d0c9ab6078e0b1d1c769d4eb1d3e3
Yuthala <dinapolousova@gmail.com>
2024-03-09 11:58:59 +0300

    Button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 import './App.css';
+import Button from './components/Button/Button';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h5>Hello world
+        <Button title={'ADD'} disable="false" type={'add'}/>
+        <Button title={'REMOVE'} disable="false" type={'remove'}/>
+        <Button title={'CHECKOUT'} disable="false" type={'checkout'}/>
+      </h5>
+
     </div>
   );
 }
